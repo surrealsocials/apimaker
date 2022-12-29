36206c585eee64b9203b33749f089a964557d5dc
surrealsocials <69706631+surrealsocials@users.noreply.github.com>
2022-12-30 04:13:14 +1100

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,18 @@
-from flask import Flask
+
+import images
+from flask import Flask,Response,request
+import openai
+openai.api_key = "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"
+
+
+def gimage(args1):
+	response = openai.Image.create(
+		prompt=args1,
+		n=1,
+		size="512x512"
+		)
+	image_url = response['data'][0]['url']
+	return image_url
 
 app = Flask(__name__)
 
@@ -7,14 +21,16 @@ def index():
 	res= "welcome to your API"
 	return res, 200, {"Access-Control-Allow-Origin": "*"}
 
-@app.route("/myfun",methods=["GET"])
-def myfun():
-	return
+@app.route("/getfun",methods=["GET"])
+def getfun():
+	args1 = request.args['text']
+	return gimage(args1), 200, {"Access-Control-Allow-Origin": "*"}
+	
 
 @app.route("/postfun",methods=["POST"])
 def postfun():
 	return
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8080 ,debug=False)
+	app.run(host='0.0.0.0', port=8080, debug=False)
 
