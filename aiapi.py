79dd658ba00ea09501c5b2a6d7c003bbf690d6eb
surrealsocials <69706631+surrealsocials@users.noreply.github.com>
2022-12-30 05:43:06 +1100

    Update aiapi.py

--- aiapi.py
+++ aiapi.py
@@ -1,7 +1,8 @@
 
-from flask import Flask,Response,request,send_file
+
+from flask import Flask,Response,request
 import openai
-openai.api_key = "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"
+openai.api_key = "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"
 
 
 def gimage(args1):
@@ -13,27 +14,26 @@ def gimage(args1):
 	image_url = response['data'][0]['url']
 	return image_url
 
+print(gimage('hello'))
+
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
 	res= "welcome to your API"
-	return send_file("index.html"), 200, {"Access-Control-Allow-Origin": "*"}
+	return res, 200, {"Access-Control-Allow-Origin": "*"}
 
 @app.route("/getfun",methods=["GET"])
 def getfun():
 	args1 = request.args['text']
-	return gimage(args1)
+	return gimage(args1), 200, {"Access-Control-Allow-Origin": "*"}
 	
 
-@app.route("/art.png",methods=["GET"])
-def art():
-	return send_file("art.png")
+@app.route("/postfun",methods=["POST"])
+def postfun():
+	return
 
-
-@app.route("/wheel.gif",methods=["GET"])
-def wheel():
-	return send_file("wheel.gif")
-
+#if __name__ == '__main__':
 app.run(host='0.0.0.0', port=8080, debug=False)
 
